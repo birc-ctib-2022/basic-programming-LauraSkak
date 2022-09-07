668a56b3c74e6b912365bd799285733fa03df7e2
Laura Skak <201805553@post.au.dk>
2022-09-07 15:19:34 +0200

    creates the pattern

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,15 @@
 
 # Print the pattern
+
+
+string = ""
+
+for i in range(1, 10, 1):
+    if i <= 5:
+        string = "* "*(i-1)
+        string = f"{string}*"
+        print(string)
+    else:
+        string = "* "*(i-1-(i-5)*2)
+        string = f"{string}*"
+        print(string)
