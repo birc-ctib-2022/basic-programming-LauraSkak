9ec54b824aededc473742c575a45b5527854d1cf
Laura Skak <201805553@post.au.dk>
2022-09-07 16:27:17 +0200

    creates script that encodes and decodes

--- src/hex.py
+++ src/hex.py
@@ -14,9 +14,19 @@ match command:
     case "encode":
         # Implement the encoding here
         encoding = ""
+        lst = x.split()
+        new_lst = []
+        for i in lst:
+            new_lst.append(hex(ord(i)))
+        encoded = new_lst.join()
         print(encoding)
 
     case "decode":
         # Implement the decoding here
         decoding = ""
+        lst = x.split('0x')
+        new_lst = []
+        for i in lst:
+            new_lst.append(chr(int(i)))
+        decoding = new_lst.join()
         print(decoding)
