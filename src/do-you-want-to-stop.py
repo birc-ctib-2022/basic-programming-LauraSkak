5d6c41ac0e7382545b16161518815bfc49c591af
Laura Skak <201805553@post.au.dk>
2022-09-07 14:52:18 +0200

    Makes a do you want to stop-whileloop

--- src/do-you-want-to-stop.py
+++ src/do-you-want-to-stop.py
@@ -1,3 +1,12 @@
 
 # You should write a loop around this question, and keep asking until
 # the answer is "yes" (lower case).
+
+answer = None
+
+while answer != "yes":
+    print("Hello, World!")
+    answer = input('Do you want to stop? ')
+    print(answer)
+    if answer == "yes":
+        break
